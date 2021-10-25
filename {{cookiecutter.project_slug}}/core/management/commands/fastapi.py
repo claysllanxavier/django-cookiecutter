c2cba34a036478b888b2380fe31d0c222e74f3e7
Claysllan Xavier <claysllan@gmail.com>
2021-10-25 10:22:32 -0300

    :sparkles: Permiter gerar schemas da app

--- {{cookiecutter.project_slug}}/core/management/commands/fastapi.py
+++ {{cookiecutter.project_slug}}/core/management/commands/fastapi.py
@@ -37,6 +37,7 @@ class AppModel:
 
     def __init__(self, path_fastapi, app_name, model_name=None):
         try:
+            self.path_core = os.path.join(self.BASE_DIR, "core")
             self.path_fastapi = path_fastapi
             self.models = None
             self.model = None
@@ -45,6 +46,7 @@ class AppModel:
             self.app = apps.get_app_config(self.app_name_lower)
             self.model_name = str(model_name).strip()
             self.model_name_lower = self.model_name.lower()
+
             if model_name is not None:
                 self.model = self.app.get_model(self.model_name)
             else:
@@ -101,10 +103,10 @@ class Command(BaseCommand):
                      "PositiveIntegerField", "PositiveSmallIntegerField", "SlugField", "SmallIntegerField", "TextField",
                      "TimeField", "URLField", "UUIDField", "ForeignKey", "OneToOneField", ]
 
-    _fastapi_types = ["int", "int", "BLANK_CHOICE_DASH", "int", "int", "String", "bool", "String", "String", "DateTime",
-                      "DateTime", "double", "int", "String", "String", "String", "String", "String", "String", "double",
-                      "String", "String", "int", "String", "String", "bool", "String", "int", "int", "String", "int",
-                      "String", "DateTime", "String", "String", "String", "int", ]
+    _schemas_types = ["int", "int", "BLANK_CHOICE_DASH", "int", "int", "str", "bool", "str", "str", "datetime.date",
+                      "datetime.datetime", "float", "int", "EmailStr", "str", "str", "str", "str", "str", "float",
+                      "str", "str", "int", "str", "str", "bool", "str", "int", "int", "str", "int",
+                      "str", "DateTime", "str", "str", "int", "int", ]
 
 
     def add_arguments(self, parser):
@@ -118,6 +120,14 @@ class Command(BaseCommand):
         """Método responsável por verificar se o diretório já existe."""
         return Utils.check_dir(path)
 
+    def _check_file(self, path):
+        """Método responsável por verificar se o arquivo já existe no caminho informado."""
+        return Utils.check_file(path)
+    
+    def __check_content(self, path, text_check):
+        """Método responsável por verificar se o texto passado com parâmetro existe no conteúdo do arquivo."""
+        return Utils.check_content(path, text_check)
+
     def __ignore_base_fields(self, field):
         """Método responsável por remover da análise do models os atributos herdados da classe pai Base
 
@@ -128,7 +138,7 @@ class Command(BaseCommand):
             bool -- True se o atributo for um dos atributos da classe pai, caso contrário False.
         """
         try:
-            __ignore_fields = ["id", "enabled", "deleted", "createdOn", "created_on", "updatedOn", "updatedOn", ]
+            __ignore_fields = ["id",  "deleted", "created_on", "updated_on" ]
             return field in __ignore_fields
         except Exception as error:
             Utils.show_message(f"Error in __ignore_base_fields: {error}", error=True)
@@ -162,10 +172,85 @@ class Command(BaseCommand):
             if not Utils.check_dir(self.fastapi_dir):
                 Utils.show_message("Criando o projeto Fastapi.")
                 __cmd_fastapi_create = "git clone https://github.com/claysllanxavier/fastapi-to-do.git {}".format(self.fastapi_dir)
+                __cmd_remove_git = "cd {} && rm -rf .git".format(self.fastapi_dir)
                 subprocess.call(__cmd_fastapi_create, shell=True)
+                subprocess.call(__cmd_remove_git, shell=True)
                 Utils.show_message("Projeto criado com sucesso.")
         except Exception as error:
             Utils.show_message(f"Error in __init_Fastapi: {error}", error=True)
+    
+    def __init_app(self, app_path):
+        """Método para iniciar o projeto Fastapi 
+        """
+        try:
+            if not Utils.check_dir(app_path):
+                Utils.show_message("Criando diretório da app")
+                os.makedirs(app_path)
+                Utils.show_message("Diretório criado com sucesso")
+        except Exception as error:
+            Utils.show_message(f"Error in __init_Fastapi: {error}", error=True)
+
+    def __apply_pep(self, path):
+        try:
+            os.system('autopep8 --in-place --aggressive --aggressive {}'.format(path))
+            os.system('isort {}'.format(path))
+        except Exception as error:
+            Utils.show_message(f"Ocorreu o erro : {error}")
+            pass
+
+    def __manage_schema(self):
+        """Método responsável por criar/configurar o arquivo de schema para a FastAPI """
+        try:
+            Utils.show_message("Trabalhando na configuração do Schema do model {}".format(self.model))
+            
+            content = self.__get_snippet(Path(
+            f"{self.path_core}/management/commands/snippets/fastapi/schema.txt"))
+            # Interpolando os dados
+            content = content.replace("$ModelClass$", self.model)
+            model = self.app_instance.get_model(self.model)
+            fields = model._meta.fields
+            result = ''
+            for field in iter(fields):
+                item = {}
+                item["app"], item["model"], item["name"] = str(field).split('.')
+                item["type"] = (str(
+                    str(type(field)).split('.')[-1:])
+                                .replace("[\"", "").replace("\'>\"]", ""))
+                if item["type"] not in self._django_types:
+                    print('Campo {} desconhecido'.format(field))
+                    continue
+                if not self.__ignore_base_fields(item['name']):
+                    attribute = self._schemas_types[self._django_types.index(item['type'])]
+                    field_name = item.get('name')
+                    if (getattr(field, 'null', None)):
+                        attribute = f"Optional[{attribute}]"
+                    if (field.get_default() is not None and field.get_default() != ""):
+                        attribute += f" = {field.get_default()}"
+                    if (item.get("type") in ('ForeignKey', 'OneToOneField')):
+                        field_name = field.get_attname_column()[1]
+                    result += f"\t {field_name}: {attribute}\n"
+            content = content.replace("$fields$", result)
+
+            # Verificando se o arquivo forms.py existe
+            if self._check_file(self.path_schema) is False:
+                # Criando o arquivo com o conteúdo da interpolação
+                with open(self.path_schema, 'w') as arquivo:
+                    arquivo.write(content)
+                self.__apply_pep(self.path_schema)
+                return
+            # Verificando se já existe configuração no forms para o
+            # Models informado
+            if self.__check_content(
+                    self.path_schema, "class {}".format(self.model)):
+                Utils.show_message("O model informado já possui schema configurado.")
+                return
+
+            with open(self.path_schema, 'a') as schema:
+                schema.write("\n")
+                schema.write(content)
+            self.__apply_pep(self.path_schema)
+        except Exception as error:
+            Utils.show_message(f"Error in __manage_schema: {error}", error=True)
 
   
 
@@ -180,7 +265,7 @@ class Command(BaseCommand):
         de estados para o projeto Fastapi foram alteradas, agora todo projeto gerado utiliza como pacote de gerência 
         de estado o pacote o Cubit/Bloc
         """
-        pass
+        self.__manage_schema()
 
     def handle(self, *args, **options):
 
@@ -202,15 +287,20 @@ class Command(BaseCommand):
         # Pegando o diretório absoluto atual do projeto.
         self.path_root = os.path.normpath(os.getcwd() + os.sep)
         # Criando o path para a APP informada.
-        self.path_app = os.path.join(self.path_root, 'fastapi', app)
+        self.path_app = os.path.join(self.fastapi_dir, app)
+        self.path_app_local = os.path.join(self.path_root, app)
         # Criando o path para a APP Core.
         self.path_core = os.path.join(self.BASE_DIR, "core")
         # Criando o path para os models baseado no App informada.
-        self.path_model = os.path.join(self.path_app, "models.py")
+        self.path_model = os.path.join(self.path_app_local, "models.py")
         # Convertendo os nomes para caracteres minúsculo.
         # para serem usado nos locais que necessitem dos nomes
         # em minúsculo.
         self.app_lower = app.lower()
+
+        # Criando o path para os forms baseado na App informada.
+        self.path_schema= os.path.join(self.path_app, "schemas.py")
+       
         # Verificando se o diretório da App informada existe
         if self._check_dir(self.fastapi_dir) is False:
             self.__init_fastapi()
@@ -223,9 +313,58 @@ class Command(BaseCommand):
             return
         
         if self._check_dir(self.path_app) is False:
-            Utils.show_message("Diretório não encontrado.")
-            return
+            self.__init_app(self.path_app)
+
         # Criando uma instancia da app
         self.app_instance = apps.get_app_config(self.app_lower)
+        # Verificando se o usuário passou o nome do model
+        if options['Model']:
+            model = options['Model'] or None
+            if Utils.contain_number(model) is False:
+                # Removendo os espaços em branco
+                self.model = model.strip()
+                # Verificando se existe no models.py o Model informado
+                if self.__check_content(
+                        self.path_model,
+                        'class {}'.format(self.model)) is False:
+                    Utils.show_message("Model informado não encontrado.")
+                    return
+            try:
+                # Verifica se o model está na app informada
+                # Se o model for abstract ela retornará uma exceção
+                # LookupError
+                self.app_instance.get_model(self.model)
+                Utils.show_message(
+                    "Gerando arquivos para o model {}".format(self.model))
+                # Convertendo os nomes para caracteres minúsculo.
+                # para serem usado nos locais que necessitem dos nomes
+                # em minúsculo.
+                self.model_lower = model.lower()
+                self.call_methods(options)
+                Utils.show_message("Processo concluído.")
+            except LookupError:
+                Utils.show_message(
+                    "Esse model é abastrato. "
+                    "Não vão ser gerados os arquivos.")
+        else:
+            # recupera todos os models da app
+            # print(self.app_instance.get_models())
+            for model in self.app_instance.get_models():
+                model = model.__name__
+                # Removendo os espaços em branco
+                self.model = model.strip()
+                Utils.show_message(
+                    "Gerando arquivos para o model {}".format(self.model))
+                # Convertendo os nomes para caracteres minúsculo.
+                # para serem usado nos locais que necessitem dos nomes
+                # em minúsculo.
+                self.model_lower = model.lower()
+                # Chama os métodos de geração de arquivos
+                self.call_methods(options)
+                Utils.show_message(
+                    "Processo concluído para o model {}.".format(
+                        self.model))
+            Utils.show_message("Processo concluído.")
+            return
 
       
