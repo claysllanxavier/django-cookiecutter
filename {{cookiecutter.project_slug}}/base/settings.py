aa31421162db45b7d2b5f237285dc6691890aacf
Guilherme de Carvalho Carneiro <guilhermecarvalhocarneiro@gmail.com>
2021-11-03 17:41:18 -0300

    Refatorando os templates da nova interface

--- {{cookiecutter.project_slug}}/base/settings.py
+++ {{cookiecutter.project_slug}}/base/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'base.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': config('DB_ENGINE'),
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
