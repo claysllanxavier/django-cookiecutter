64d73e8550d4c0cebcdd0a04bab509d2a6be8c89
Guilherme de Carvalho Carneiro <guilhermecarvalhocarneiro@gmail.com>
2021-11-05 13:54:44 -0300

    Removendo a app financeiro do api.py

--- {{cookiecutter.project_slug}}/core/management/commands/snippets/fastapi_project/core/api.py
+++ {{cookiecutter.project_slug}}/core/management/commands/snippets/fastapi_project/core/api.py
@@ -2,14 +2,13 @@ from fastapi import APIRouter
 
 from .config import settings
 from authentication.api import router as router_users
-from financeiro.api import router as router_financeiro
 
-'''
+"""
+TODO Importar para esse arquivo os endpoints das apps do Projeto
 Arquivos com os endpoints principais do projeto
 
 - Deve ser importado nesse arquivo, os arquivo routers de todas as apps
-'''
+""" 
 
 api_router = APIRouter(prefix=settings.api_str)
 api_router.include_router(router_users, prefix="/authentication")
-api_router.include_router(router_financeiro, prefix="/financeiro")
